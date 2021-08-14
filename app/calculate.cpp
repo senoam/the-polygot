8282596924c6d6f8c78cc86601ef820b00c5404f
senoam <seno.adhimuhammad@gmail.com>
2021-08-14 12:02:51 +0700

    Final Commit 2.0

--- app/calculate.cpp
+++ app/calculate.cpp
@@ -1,23 +1,23 @@
-// #include <iostream>
-// #include "stdio.h"
-// using namespace std;
+#include <iostream>
+#include "stdio.h"
+using namespace std;
 
 
-// extern "C" int my_print(int a) {
-//         cout <<"Inside print";
-//         return 1;
-//     }
+extern "C" int my_print(int a) {
+        cout <<"Inside print";
+        return 1;
+    }
 
-// float calculate_avg(int* arr, int size) {
-//     int sum = 0;
-//     cout << size << endl;;
-//     for(int i = 0; i < size; i++) {
-//         cout << arr[i] << " ";
-//         sum = sum + arr[i];
-//     }
-//     return sum/size;
+float calculate_avg(int* arr, int size) {
+    int sum = 0;
+    cout << size << endl;;
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+        sum = sum + arr[i];
+    }
+    return sum/size;
 
-// }
+}
 
 extern "C" int takeRange(int a, int b) {
         return a-b;
